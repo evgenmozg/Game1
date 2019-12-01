0ce91714bd49ace68db2c232e70de5f81f7bb28c
evgenmozg <evgenmozg07@yandex.ru>
2019-12-01 15:22:45 +1000

    1 занятие

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+WIN_WIDTH, WIN_HEIGHT = 780, 630
+BG_COLOR = (192, 192, 192)
+
+pygame.init()
+pygame.display.set_caption('первая игра')
+screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+
+run = True
+while run:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            run = False
+
+    screen.fill(BG_COLOR)
+    pygame.display.update()
